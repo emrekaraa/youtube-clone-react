1031106d8b7b7643280ac097709f155a38184785
Emre Kara <emrekara.dev@gmail.com>
2022-09-17 16:19:06 +0300

    search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import SearchCard from "../components/SearchCard";
+import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
+import { clearVideos } from "../store";
+import { useAppSelector } from "../store/hooks";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import { HomePageVideos } from "../Types";
 
 export default function Search() {
-  return <div>Search</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  useEffect(() => {
+    dispatch(clearVideos());
+    if (searchTerm === "") {
+      navigate("/");
+    } else {
+      dispatch(getSearchPageVideos(false));
+    }
+  }, [dispatch, navigate, searchTerm]);
+
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <div className="py-8 pl-8 flex flex-col gap-5 w-full">
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={() => dispatch(getSearchPageVideos(true))}
+              hasMore={videos.length < 500}
+              loader={<Spinner />}
+              height={600}
+            >
+              {videos.map((item: HomePageVideos, key) => (
+                <div className="my-5">
+                  <SearchCard data={item} key={key} />
+                </div>
+              ))}
+            </InfiniteScroll>
+          </div>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { InitialState } from "../Types";
 import { getHomePageVideos } from "./reducers/getHomePageVideos";
+import { getSearchPageVideos } from "./reducers/getSearchPageVideos";
 
 const initialState: InitialState = {
   videos: [],
@@ -19,9 +20,20 @@ const YoutubeSlice = createSlice({
       state.videos = [];
       state.nextPageToken = null;
     },
+    changeSearchTerm: (state, action: PayloadAction<string>) => {
+      state.searchTerm = action.payload;
+    },
+    clearSearchTerm: (state) => {
+      state.searchTerm = "";
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(getHomePageVideos.fulfilled, (state, action) => {
+      state.videos = action.payload.parsedData;
+      state.nextPageToken = action.payload.nextPageToken;
+    });
+
+    builder.addCase(getSearchPageVideos.fulfilled, (state, action) => {
       state.videos = action.payload.parsedData;
       state.nextPageToken = action.payload.nextPageToken;
     });
@@ -34,7 +46,7 @@ export const store = configureStore({
   },
 });
 
-export const { clearVideos } = YoutubeSlice.actions;
+export const { clearVideos, changeSearchTerm, clearSearchTerm } = YoutubeSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

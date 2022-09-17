b9b5dee9df30fab47801f93b62f635c4b3dbf2dd
Emre Kara <emrekara.dev@gmail.com>
2022-09-17 18:05:21 +0300

    watch

--- src/Types.ts
+++ src/Types.ts
@@ -1,6 +1,6 @@
 export interface InitialState {
   videos: HomePageVideos[];
-  currentPlaying: CurrentPlaying | null;
+  currentPlaying: any;
   searchTerm: string;
   searchResutls: [];
   nextPageToken: string | null;
@@ -22,5 +22,44 @@ export interface HomePageVideos {
     name: string;
   };
 }
-export interface CurrentPlaying {}
-export interface RecommendedVideos {}
+export interface CurrentPlaying {
+  videoId?: string;
+  videoTitle?: string;
+  videoDescription?: string;
+  videoViews?: string | number;
+  videoLikes?: string | number;
+  videoAge?: string;
+  channelInfo?: {
+    id?: string;
+    image?: string;
+    name?: string;
+    subscribers?: string | number;
+  };
+}
+export interface RecommendedVideos {
+  videoId: string;
+  videoTitle: string;
+  videoThumbnail: string;
+  videoDuration: string;
+  videoViews: string | number;
+  videoAge: string;
+  channelInfo: {
+    id: string;
+    name: string;
+  };
+}
+
+export interface Item {
+  snippet: {
+    title: string;
+    thumbnails: { medium: { url: string } };
+    publishedAt: Date;
+    channelTitle: string;
+    channelId: string;
+  };
+  contentDetails: {
+    upload: {
+      videoId: string;
+    };
+  };
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { parseVideoDuration } from "./parseVideoDuration";
 export { convertRawViewstoString } from "./convertRawViewsToString";
 export { timeSince } from "./timeSince";
 export { parseData } from "./parseData";
+export { parseRecommendedData } from "./parseRecommendedData";
